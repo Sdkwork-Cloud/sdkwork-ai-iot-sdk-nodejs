515f7f54f28d3577a563b17d52966ac0160598a7
zowork <luolishu@zowalk.com>
2025-11-01 00:04:01 +0800

    fixed

--- examples/vue-example/dist/prod/js/_id_-pzT-Hlba.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-DS3FT4GI.js";import"./vue-vendor-CnVaDBlt.js";import"./utils-vendor-WEzSe-CR.js";const e=r({},[["render",function(r,e){return null}]]);export{e as default};
---
+++ examples/vue-example/dist/prod/js/products-Dt0nITx0.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-Czm3MDyH.js";import"./vue-vendor-CnVaDBlt.js";import"./utils-vendor-WEzSe-CR.js";const e=r({},[["render",function(r,e){return null}]]);export{e as default};
